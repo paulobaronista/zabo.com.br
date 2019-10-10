c5853fb07969275761728bde3b932efa7dd399ca
PauloBaronista <paulo@rpm.com.br>
2019-10-10 00:09:49 -0300

    Criando layout

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,20 +1,189 @@
-<div id="nosso-cafe" class="nosso-cafe">
+<div id="home" class="home">
     <section class="carousel-default">
         <div id="carousel-default" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner" role="listbox">
                 <div class="item active">
                     <img class="img-responsive" src="<?= base_url(); ?>assets/images/banner-1.jpg" alt="Banner">
                 </div>
-                <div class="item">
+                <!-- <div class="item">
                     <img class="img-responsive" src="<?= base_url(); ?>assets/images/banner-1.jpg" alt="Banner">
                 </div>
                 <div class="item">
                     <img class="img-responsive" src="<?= base_url(); ?>assets/images/banner-1.jpg" alt="Banner">
-                </div>
+                </div> -->
             </div>
         </div>
     </section>
 </div>
+
+<div id="jardins" class="jardins">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center">
+                <h2>Jardins.</h2>
+                <p>uma síntese do que São Paulo tem<br>
+                    <span>de mais charmoso e fascinante.</span></p>
+                <span class="line"></span>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 info text-center">
+                <p>Com mais de XX empreendimentos lançados no bairro, a Zabo é a construtora
+                    de altíssimo padrão que mais conhece os Jardins. Sabe exatamente onde estão
+                    os melhores terrenos e as melhores oportunidades para você investir nesse lugar
+                    tão desejado e privilegiado. Veja a seguir os próximos lançamentos da Zabo na região.
+                    Escolha o seu e viva no endereço que é o símbolo do que a capital paulista
+                    oferece de melhor. Você merece.</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 projetos">
+                <div class="col-xs-12 col-sm-12 col-md-12 padding-off">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Shopping-Cidade-Sao-Paulo.png" alt="Shopping Cidade São Paulo">
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 padding-off">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Lelis-Blanc.png" alt="Lelis Blanc">
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 padding-off">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Galeria-Zipper.png" alt="Galeria Zipper">
+                </div>
+                <div class="col-xs-12 col-sm-5 col-md-5 padding-off">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Pettirosso-Ristorante.png" alt="Pettirosso Ristorante">
+                </div>
+                <div class="col-xs-12 col-sm-7 col-md-7 padding-off">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Fratina.png" alt="Fratina">
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 padding-off">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Casa-Santa-Luzia.png" alt="Casa Santa Luzia">
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 padding-off">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/DOM-Restaurante.png" alt="DOM Restaurante">
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 padding-off">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Chef-Rouge-Bistrot.png" alt="Chef Rouge Bistrot">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="lancamentos" class="lancamentos">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center">
+                <h2>lançamentos</h2>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
+                <div class="col-xs-12 col-sm-6 col-md-6 mb-20">
+                    <img class="img-responsive center-block mb-10" src="<?= base_url(); ?>assets/images/Maison-Helena.png" alt="Maison Helena">
+                    <div class="col-xs-12">
+                        <p class="txt-1">Al. Franca, 1633</p>
+                        <p class="txt-1">Jardins</p>
+                        <p class="txt-2">208 e 312m² - 3 e 4 Suítes/4 Vagas</p>
+                        <div class="col-xs-12 padding-off text-center">
+                            <p class="txt-3">Entrega Prevista para jul/22</p>
+                            <a href="">Saiba Mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 mb-20">
+                    <img class="img-responsive center-block mb-10" src="<?= base_url(); ?>assets/images/Maison-Marie.png" alt="Maison Marie">
+                    <div class="col-xs-12">
+                        <p class="txt-1">Rua José Maria Lisboa, 1326</p>
+                        <p class="txt-1">Jardins</p>
+                        <p class="txt-2">205m² - 3 Suítes/4 Vagas</p>
+                        <div class="col-xs-12 padding-off text-center">
+                            <p class="txt-3">Entrega Prevista para jan/20</p>
+                            <a href="">Saiba Mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 mb-20">
+                    <img class="img-responsive center-block mb-10" src="<?= base_url(); ?>assets/images/Maison-Giselle.png" alt="Maison Giselle">
+                    <div class="col-xs-12">
+                        <p class="txt-1">Rua Peixoto Gomide, 1389</p>
+                        <p class="txt-1">Jardins</p>
+                        <p class="txt-2">226m² - 4 Dormitórios/4 Vagas</p>
+                        <div class="col-xs-12 padding-off text-center">
+                            <p class="txt-3">Entrega Prevista para jan/22</p>
+                            <a href="">Saiba Mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 mb-20">
+                    <img class="img-responsive center-block mb-10" src="<?= base_url(); ?>assets/images/Maiso-Chloe.png" alt="Maiso Chloe">
+                    <div class="col-xs-12">
+                        <p class="txt-1">Al. Casa Branca, 939</p>
+                        <p class="txt-1">Jardins</p>
+                        <p class="txt-2">174m² - 3 Suítes/3 Vagas</p>
+                        <div class="col-xs-12 padding-off text-center">
+                            <p class="txt-3">Entrega Prevista para jul/22</p>
+                            <a href="">Saiba Mais</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="lancamentos-info">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center">
+                <h2>VOCÊ MERECE O MELHOR.<br>
+                    VOCÊ MERECE UM ZABO.</h2>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
+                <div class="hidden-xs col-sm-4 col-md-4">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/localizacao.png" alt="Localização">
+                </div>
+                <div class="hidden-xs col-xs-4 col-sm-4 col-md-4">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/arquitetura.png" alt="Arquitetura">
+                </div>
+                <div class="hidden-xs col-xs-4 col-sm-4 col-md-4">
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/construcao.png" alt="Construção">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container box-info">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center">
+            <div class="col-xs-12 col-sm-4 col-md-4 mb-10">
+                <img class="img-responsive center-block visible-xs" src="<?= base_url(); ?>assets/images/localizacao.png" alt="Localização">
+                <h2>localização</h2>
+                <p>Atuamos nas regiões
+                    mais nobres de São Paulo, sempre buscando
+                    os endereços mais
+                    exclusivos.</p>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 mb-10">
+                <img class="img-responsive center-block visible-xs" src="<?= base_url(); ?>assets/images/arquitetura.png" alt="Arquitetura">
+                <h2>arquitetura</h2>
+                <p>Todos os nossos
+                    empreendimentos são
+                    planejados para oferecer
+                    absoluta funcionalidade
+                    em todos os espaços, onde prevalecem sempre
+                    o bom gosto, o requinte
+                    e a sofisticação Zabo.</p>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <img class="img-responsive center-block visible-xs" src="<?= base_url(); ?>assets/images/construcao.png" alt="Construção">
+                <h2>construção</h2>
+                <p>A Zabo preza pela
+                qualidade de construção de seus empreendimentos, oferecendo acabamentos
+                de altíssimo padrão
+                com máxima tecnologia.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
 
 
 <div id="contato" class="contato animated wow slideInUp">
