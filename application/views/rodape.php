eb415ca94cca25573f9f809c3c7d5e4ea6bdc6e5
PauloBaronista <paulo@rpm.com.br>
2019-11-01 21:52:39 -0300

    Criando link what

--- application/views/rodape.php
+++ application/views/rodape.php
@@ -1,1 +1,3 @@
-
+<a class="whats" href="https://api.whatsapp.com/send?1=pt_BR&phone=5511996100076" target="_blank">
+    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/whatsapp.png" alt="Whatsapp">
+</a>
